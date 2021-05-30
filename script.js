63b42ab59dd7ac5a87fc3149a7f3fda28dcc329f
ilyabalukov <81466094+ilyabalukov@users.noreply.github.com>
2021-05-30 15:00:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+function equal()
+{
+    d=document
+    var a = Number(d.form1.A.value);
+    var b = Number(d.form1.B.value);
+    var q;
+    if (a===b)
+    {
+        q="Числа равны";
+    }
+    else {q="Числа не равны";}
+    d.form1.q.value=q;
+}
+
+
+function Show()
+{
+    d=document
+    var admin, name;
+    name = "Ilya";
+    admin = name;
+    alert(admin);
+}
+
+function Age()
+{
+    d=document
+    do {age = +prompt("Укажите ваш возраст");}
+    while 
+    (confirm("Вы уверены, что вам " +age)===false)
+    alert("Вам " +age);
+}
+
+function Com()
+{
+    d=document
+    let ok = confirm('Отправить отзыв о сайте?')
+    if (ok) {
+        alert('Спасибо за отзыв');
+    } else {
+        alert('Отзыв не отправлен!');
+    }
+}
